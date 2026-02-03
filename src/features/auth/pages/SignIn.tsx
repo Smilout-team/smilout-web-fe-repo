a6c51a3482862aa4c2ab9848eed61d40c8bf6b79
thidieu2005 <thidieu100625@gmail.com>
2026-02-03 16:23:55 +0700

    feat(SMILOUT-2): done UI sign in for consumer

--- src/features/auth/pages/SignIn.tsx
+++ src/features/auth/pages/SignIn.tsx
@@ -1,5 +1,20 @@
-const SignIn = () => {
-  return <div>Sign In Page</div>;
+import { SignInHeader } from '../components/SignInHeader';
+import { SignInForm } from '../components/SignInForm';
+
+export function SignIn() {
+  return (
+    <div className="flex min-h-screen items-center justify-center sm:p-6">
+      <div className="flex min-h-screen w-full max-w-[393px] flex-col overflow-hidden bg-white sm:min-h-fit sm:rounded-[40px] md:max-w-[700px]">
+        <div className="scrollbar-hide flex-1 overflow-y-auto px-6 py-12 md:px-12">
+          <SignInHeader />
+
+          <div className="h-[32px]" />
+
+          <SignInForm />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default SignIn;
--- src/features/auth/routes/AuthRoutes.tsx
+++ src/features/auth/routes/AuthRoutes.tsx
@@ -1,16 +1,14 @@
-import {type RouteObject} from 'react-router-dom';
+import { type RouteObject } from 'react-router-dom';
 import SignIn from '@/features/auth/pages/SignIn';
 import Unauthorized from '@/features/auth/pages/Unauthorized';
 
 export const AuthRoutes: RouteObject[] = [
-    {
-        path: 'sign-in',
-        element: (
-            <SignIn />
-        )
-    },
-    {
-        path: '/unauthorized',
-        element: <Unauthorized />
-    }
+  {
+    path: 'sign-in',
+    element: <SignIn />,
+  },
+  {
+    path: 'unauthorized',
+    element: <Unauthorized />,
+  },
 ];
