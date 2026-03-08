40d0209f13851af927b8c7ca12aa19eafd21ea34
KimHan2074 <ngothikimhan.2004@gmail.com>
2026-03-08 12:42:14 +0700

    feat(SMILOUT-69): build the ui for the store staff orders management page

---
+++ src/features/store-staff/orders-management/components/OrderStatusTag.tsx
@@ -0,0 +1,36 @@
+import Tag from '@/shared/components/common/Tag';
+import type { OrderStatus } from '../types/order.type';
+
+import { Clock, ChefHat, CheckCircle2, XCircle } from 'lucide-react';
+
+export default function OrderStatusTag({ status }: { status: OrderStatus }) {
+  switch (status) {
+    case 'PENDING':
+      return (
+        <Tag tone="primary" icon={<Clock />}>
+          Chờ xác nhận
+        </Tag>
+      );
+
+    case 'PREPARING':
+      return (
+        <Tag tone="blue" icon={<ChefHat />}>
+          Đang chuẩn bị
+        </Tag>
+      );
+
+    case 'COMPLETED':
+      return (
+        <Tag tone="green" icon={<CheckCircle2 />}>
+          Đã hoàn thành
+        </Tag>
+      );
+
+    case 'REJECTED':
+      return (
+        <Tag tone="red" icon={<XCircle />}>
+          Đã từ chối
+        </Tag>
+      );
+  }
+}
