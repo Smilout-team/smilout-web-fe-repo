197a8ed18b5542079e88d0956e582d5534372562
Duc Tai <voductaitxqt123@gmail.com>
2026-03-10 10:42:32 +0700

    feat(SMILOUT-20): integrated view and process order UI with api

--- src/features/store-staff/orders-management/types/order.type.ts
+++ src/features/store-staff/orders-management/types/order.type.ts
@@ -1,4 +1,10 @@
-export type OrderStatus = 'PENDING' | 'PREPARING' | 'COMPLETED' | 'REJECTED';
+export type OrderStatus =
+  | 'PENDING'
+  | 'PREPARING'
+  | 'PAID'
+  | 'COMPLETED'
+  | 'REJECTED';
+export type OrderType = 'DELIVERY' | 'INSTORE';
 
 export interface OrderItem {
   id: string;
@@ -12,6 +18,7 @@ export interface Order {
   id: string;
   customerName: string;
   phone: string;
+  deliveryPhoneNumber?: string;
   address?: string;
   items: OrderItem[];
   subtotal: number;
@@ -19,6 +26,7 @@ export interface Order {
   total: number;
   time: string;
   status: OrderStatus;
+  orderType: OrderType;
   paymentMethod: string;
   isPaid?: boolean;
   note?: string;
